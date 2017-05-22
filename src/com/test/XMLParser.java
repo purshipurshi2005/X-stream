1bd7b31717d6f7ebd4fa9f2a2796af3e7fbf9898
Purushottam <purshipurshi2005@gmail.com>
2017-05-22 11:04:39 -0400

    Update XMLParser.java

--- src/com/test/XMLParser.java
+++ src/com/test/XMLParser.java
@@ -1,7 +1,7 @@
-package com.test;
-
+package com.citi;
 
 import java.io.IOException;
+import java.io.StringWriter;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -10,45 +10,72 @@ import javax.xml.parsers.ParserConfigurationException;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
+import com.sun.org.apache.xml.internal.serialize.OutputFormat;
 import com.sun.org.apache.xml.internal.serialize.XMLSerializer;
 
 public class XMLParser {
 
-public static void main(String[] args) {
-DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+	public static void main(String[] args) {
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 
-    DocumentBuilder loader=null;
-try {
-loader = factory.newDocumentBuilder();
-} catch (ParserConfigurationException e) {
-// TODO Auto-generated catch block
-e.printStackTrace();
-}
+		DocumentBuilder loader = null;
+		try {
+			loader = factory.newDocumentBuilder();
+		} catch (ParserConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
-    Document document = loader.newDocument();
+		Document document1 = loader.newDocument();
+		//<?xml version="1.0"?>	<order1> purshi & "" Thimmappa </order1>
+		Element order1 = document1.createElement("order1");
+		order1.setAttribute("ABC", "\"Hi");
+		order1.setTextContent("purshi & \"'Thimmappa");
+		document1.appendChild(order1);
 
-    Element order = document.createElement("order");
-    order.setAttribute("recipientID", "Purushottam&yahoo.com");
-    order.setTextContent("<?xml version=\"1.0\"?><order>pursht&lt;&amp;<purshi</order");
+	    OutputFormat format = new OutputFormat(document1, "UTF-8", true);
+	    format.setIndenting(true);
+	    format.setPreserveSpace(false);
+	    format.setLineWidth(500);
 
-    document.appendChild(order);
+		String xmlString = new String();
+		StringWriter stringOut = new StringWriter();
 
-    XMLSerializer serializer = new XMLSerializer();
-    try {
-    	
-   serializer.setOutputCharStream(new java.io.FileWriter("C:\\var\\order.xml"));
-    	
-    serializer.serialize(document);
-   
-    } catch (IOException e) {
-// TODO Auto-generated catch block
-e.printStackTrace();
-}
+		XMLSerializer serializer1 = new XMLSerializer(stringOut, format);
+		try {
 
-// TODO Auto-generated method stub
+			serializer1.serialize(document1);
+			xmlString = stringOut.toString();
+			System.out.println(xmlString);
+			} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
-}
+	
+		
+		Document document = loader.newDocument();
+		//<?xml version="1.0"?>	<order> <?xml version="1.0"?>	<order1> purshi & Thimmappa </order1> </order>
+		Element order = document.createElement("order");
+			order.setTextContent(xmlString);
 
+		document.appendChild(order);
 
+		XMLSerializer serializer = new XMLSerializer();
+		try {
+
+			serializer.setOutputCharStream(new java.io.FileWriter(
+					"C:\\var\\order.xml"));
+
+			serializer.serialize(document);
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		// TODO Auto-generated method stub
+
+	}
 
 }
